b4a7b4fed1046e6b704c0b6167f350807c9028bc
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 19:52:29 +0500

    Add files via upload

---
+++ 42. Great Magicians/GreatMagicians.js
@@ -0,0 +1,17 @@
+"use strict";
+// Great Magicians: Start with a copy of your program from Exercise 39. 
+let magicians = ['Harry Houdini', 'David Copperfield', 'Criss Angel'];
+function show_magicians(magicians) {
+    for (let i of magicians) {
+        console.log(i);
+    }
+}
+// Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. 
+function make_great(magicians) {
+    const greatMagicians = magicians.map(magician => `The Great Magician ${magician}`);
+    return (greatMagicians);
+}
+const greatMagiciansNames = make_great(magicians);
+// Call show_magicians() to see that the list has actually been modified.
+console.log("\nModified List.");
+show_magicians(greatMagiciansNames);
