bf1a3e0566c2d5423ba188a2b70fe810afcd3a86
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 20:10:54 +0500

    Delete 3.Name Cases directory

--- 3.Name Cases/ex3.ts
+++
@@ -1,6 +0,0 @@
-// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-let personName2:string = "bilal ahmed"
-console.log("lowercase:", personName2.toLowerCase())
-console.log("uppercase:", personName2.toUpperCase())
-console.log("lowercase:", personName2.replace(/\b\w/g,c=> c.toUpperCase()))
