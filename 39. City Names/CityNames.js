0a6c15ada7b7c660a31b82fdad1351f523fea926
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 19:49:09 +0500

    Add files via upload

---
+++ 39. City Names/CityNames.js
@@ -0,0 +1,10 @@
+"use strict";
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+// "Lahore, Pakistan"
+function city_country(city, country) {
+    console.log(`"${city}, ${country}"`);
+}
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+city_country("Karachi", "Pakistan");
+city_country("Lahore", "Pakistan");
+city_country("Islamabad", "Pakistan");
