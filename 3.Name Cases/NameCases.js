a6308e8b0d7dfa57c45c34d8ddd86f29c6251be3
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 20:11:14 +0500

    Add files via upload

---
+++ 3.Name Cases/NameCases.js
@@ -0,0 +1,6 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName2 = "bilal ahmed";
+console.log("lowercase:", personName2.toLowerCase());
+console.log("uppercase:", personName2.toUpperCase());
+console.log("lowercase:", personName2.replace(/\b\w/g, c => c.toUpperCase()));
