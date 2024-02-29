cb5961fbfd85aa047f5846c37c14fd5bad90d571
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 19:53:11 +0500

    Add files via upload

---
+++ 43. Unchanged Magicians/UnchangedMagicians.ts
@@ -0,0 +1,21 @@
+// Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+
+let magicians: string[] =['Harry Houdini', 'David Copperfield', 'Criss Angel'];
+function show_magicians(magicians: string[]): void{
+    for(let i of magicians){
+        console.log(i);
+    }
+}
+// Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. 
+function make_great(magicians: string[]): string[] {
+    const greatMagicians = magicians.map(magician => `The Great Magician ${magician}`);
+    return (greatMagicians)
+}
+const greatMagiciansNames: string [] = make_great(magicians)
+// Original magician list.
+console.log("\nOriginal List.")
+show_magicians(magicians);
+
+// Call show_magicians() to see that the list has actually been modified.
+console.log("\nModified List.")
+show_magicians(greatMagiciansNames)
