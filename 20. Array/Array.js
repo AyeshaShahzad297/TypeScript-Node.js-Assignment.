eb8195086dbd74da57693535a3d4f3f62c65b5ad
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 18:12:56 +0500

    Add files via upload

---
+++ 20. Array/Array.js
@@ -0,0 +1,10 @@
+"use strict";
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+let items = [];
+// Write a program that creates a list containing these items.
+items.push("K2");
+items.push("Indus River");
+items.push("Pakistan");
+items.push("Karachi");
+items.push("Urdu");
+console.log(items);
