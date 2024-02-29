012f7c70c8022c2c5247355dba71232da1994d53
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 18:22:29 +0500

    Add files via upload

---
+++ 23. Conditional Tests/ConditionalTests.js
@@ -0,0 +1,39 @@
+"use strict";
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = "Ford";
+let color = "black";
+// Test 1
+console.log("Is car == 'Ford'? I predict true.");
+console.log(car == "Ford");
+// Test 2
+console.log("Is car !== 'Toyota'? I predict true.");
+console.log(car !== "Toyota");
+// Test 3
+console.log("Is color == 'black'? I predict true.");
+console.log(color == "black");
+// Test 4
+console.log("Is color !== 'white'? I predict true.");
+console.log(color !== "white");
+// Test 5 
+console.log("Is color !== 'brown'? I predict true.");
+console.log(color !== "brown");
+// Test 6
+console.log("Is car !== 'Ford'? I predict false.");
+console.log(car !== "Ford");
+// Test 7
+console.log("Is car == 'Toyota'? I predict false.");
+console.log(car == "Toyota");
+// Test 8
+console.log("Is color !== 'black'? I predict false.");
+console.log(color !== "black");
+// Test 9
+console.log("Is color == 'white'? I predict false.");
+console.log(color == "white");
+// Test 10
+console.log("Is color == 'brown'? I predict false.");
+console.log(color == "brown");
