acdcba9de1f6e9baf828e9349b6c39dcc84db84e
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 17:31:45 +0500

    Add files via upload

---
+++ 12. Greetings/Greetings.js
@@ -0,0 +1,8 @@
+"use strict";
+//12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let friends = ["Arisha", "Ayesha", "Sarah", "Summaiya"];
+let message = "Hi, nice to meet you.";
+console.log(friends[0], `${message}`);
+console.log(friends[1], `${message}`);
+console.log(friends[2], `${message}`);
+console.log(friends[3], `${message}`);
