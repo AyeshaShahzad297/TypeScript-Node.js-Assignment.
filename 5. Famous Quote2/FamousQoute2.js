b8119aa81874fb80d400679fde5d0f1b94ed012c
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 17:26:49 +0500

    Add files via upload

---
+++ 5. Famous Quote2/FamousQoute2.js
@@ -0,0 +1,5 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famousPerson = "Eleanor Roosevelt";
+let message = "The future belongs to those who believe in the beauty of their dreams.";
+console.log(`${famousPerson} once said, "${message}"`);
---
+++ 5. Famous Quote2/FamousQuote2.ts
@@ -0,0 +1,5 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famousPerson:string = "Eleanor Roosevelt"
+let message:string = "The future belongs to those who believe in the beauty of their dreams."
+console.log(`${famousPerson} once said, "${message}"`)
