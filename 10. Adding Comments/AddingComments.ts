6847b400abebc382bbdeb298725b19744647ecbf
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 17:30:03 +0500

    Add files via upload

---
+++ 10. Adding Comments/AddingComments.ts
@@ -0,0 +1,15 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Author: Ayesha Shahzad
+// Date 25th Feb, 2024.
+// Ex1.
+let favoriteNumber:number = 5;
+// Here this program is printing the value present in the variable called favoriteNumber using template literal.
+console.log(`My favorite number is "${favoriteNumber}".`);
+
+//Ex2.
+// Here we are using arithmetic operators for this exercise.
+console.log(7+1) //Addition
+console.log(11-3) //Subtraction
+console.log(4*2) //Multiplication
+console.log(16/2) //Division
