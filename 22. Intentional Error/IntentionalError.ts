193f334a798f12895da542ef179dac387309ce4e
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 18:21:26 +0500

    Add files via upload

---
+++ 22. Intentional Error/IntentionalError.ts
@@ -0,0 +1,6 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+let newArray:string[] = ["Hamza", "Abdullah", "Tayyab", "Hasnain"];
+// console.log(newArray[5]);
+// console.log(newArray{0});
+console.log(newArray[1]);
