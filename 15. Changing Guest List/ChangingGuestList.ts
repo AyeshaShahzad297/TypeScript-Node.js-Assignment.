4e51ac47bb9d8aa72d3faca7a5ae11bd27ea4de0
AyeshaShahzad297 <160212562+AyeshaShahzad297@users.noreply.github.com>
2024-02-29 17:35:22 +0500

    Add files via upload

---
+++ 15. Changing Guest List/ChangingGuestList.ts
@@ -0,0 +1,18 @@
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+let guestList:string[] = ["Qasim", "Ali", "Abdullah"]
+for(let i=0; i<guestList.length; i++){
+    console.log("Mr. " + guestList[i] + ",\n\nYou are invited to dinner.\n\nThank You.\n")
+}
+
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+let absentGuest:string = "Qasim";
+let newGuest:string = "Ahmed";
+console.log(`Mr. ${absentGuest} can't come to the dinner.`)
+
+// • Print a second set of invitation messages, one for each person who is still in your list.
+guestList[0] = newGuest;
+for(let i=0; i<guestList.length; i++){
+    console.log("Mr. " + guestList[i] + ",\n\nYou are invited to dinner.\n\nThank You.\n\n")
+}
